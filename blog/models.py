ed40033576b35c82445ff68b17e3b7b73f23c0a0
unknown <somayeh.nezamdoost1400@gmail.com>
2025-08-23 09:50:47 +0330

    add models in 1 shahrivar

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Post(models.Model):
      title =models.CharField(max_length=100)
      content = models.TextField()
+     content_views=models.IntegerField(default=0)
+     status=models.BooleanField(default=False)
+     publish_date = models.DateTimeField(null=True)
+     created_date=models.DateTimeField(auto_now_add=True)
+     update_date=models.DateTimeField(auto_now=True)
--- website/models.py
+++ website/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Contact(models.Model):
+    name=models.CharField(max_length=255)
+    email=models.EmailField()
+    subject=models.CharField(max_length=200)
+    message=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
